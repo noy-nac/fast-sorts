66990a2ba49868fa8beba43f0a5a96686dec82a4
Canyon Mooney <canyon.mooney@gmail.com>
2024-03-04 08:39:14 -0600

    starter code

---
+++ FastSorts.java
@@ -0,0 +1,43 @@
+
+
+public class FastSorts {
+
+    // MERGE
+    /* TODO state the best/worst/avergae time complexity AND EXPLAIN WHY
+       TODO state the pre- and post-conditions for this method */
+
+    public static int[] merge(int[] left, int[] right) {
+        // TODO implement the merge algorithm
+        return new int[]{};
+    }
+
+    // TOP DOWN MERGE SORT
+    /* TODO state the best/worst/avergae time complexity AND EXPLAIN WHY
+       TODO state the pre- and post-conditions for this method */
+
+    public static void tdMergeSort(int[] array, int start, int end) {
+        // TODO implement top-down merge sort
+    }
+
+    // BOTTOM UP MERGE SORT
+    /* TODO state the best/worst/avergae time complexity AND EXPLAIN WHY
+       TODO state the pre- and post-conditions for this method */
+
+    public static void buMergeSort(int[] array) {
+        // TODO implement bottom-up merge sort
+    }
+
+    // QUICKSORT
+    /* TODO state the best/worst/avergae time complexity AND EXPLAIN WHY
+       TODO state the pre- and post-conditions for this method */
+
+    public static void quicksort(int[] array, int start, int end) {
+        // TODO implement quicksort
+    }
+
+
+    public static void main(String[] args) {
+        // TODO write test cases for each method
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+## 11.3 Fast Sorts
+
+Implement (1) top-down merge sort, (2) bottom-up merge sort, (3) quicksort, (4) write test cases for each algorithm, and (5) state the best/worst/average case time complexity of each algorithm.
+
