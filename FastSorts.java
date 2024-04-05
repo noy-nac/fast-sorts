5a7160f7cbfb307eb8fa0ecf36656d3612dcbc92
Canyon Mooney <canyon.mooney@gmail.com>
2024-04-05 11:43:47 -0500

    update starter

--- FastSorts.java
+++ FastSorts.java
@@ -8,29 +8,6 @@ public class FastSorts {
 
     public static int[] merge(int[] left, int[] right) {
         // TODO implement the merge algorithm
-        int[] result = new int[left.length+right.length];
-
-        int pos1 = 0, pos2 = 0;
-
-        for(int i = 0; i < result.length; i++) {
-            if(left[pos1] >= right[pos2]) {
-                result[i] = right[pos2];
-                if(pos2 < right.length) {
-                    pos2++;
-                }
-            }
-            else if(left[pos1] < right[pos2]) {
-                result[i] = left[pos1];
-                if(pos1 < left.length) {
-                    pos1++;
-                }
-            }
-        }
-
-        if(left[0] > right[0]) {
-        }
-
-        return new int[]{};
     }
 
     // TOP DOWN MERGE SORT
@@ -39,7 +16,6 @@ public class FastSorts {
 
     public static void tdMergeSort(int[] array, int start, int end) {
         // TODO implement top-down merge sort
-        int len1 = 0
     }
 
     // BOTTOM UP MERGE SORT
